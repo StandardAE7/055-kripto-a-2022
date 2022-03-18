14ddad32ba7af4b6aab8a7681da51bb1bd6991ef
StandardAE7 <bfm981@gmail.com>
2022-03-18 09:52:40 +0700

    Tugas 1

---
+++ Tugas1_Banabil Fawazaim Muhammad_5027201055/HEX.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import sys
+import base64
+
+ord = "63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d"
+
+byt = bytes.fromhex(ord)
+print(byt)
---
+++ Tugas1_Banabil Fawazaim Muhammad_5027201055/HextoBase64.py
@@ -0,0 +1,9 @@
+import sys
+import base64
+
+ord = "72bca9b68fc16ac7beeb8f849dca1d8a783e8acf9679bf9269f7bf"
+
+ord = bytes.fromhex(ord)
+
+ans = base64.b64encode(ord)
+print(ans)
