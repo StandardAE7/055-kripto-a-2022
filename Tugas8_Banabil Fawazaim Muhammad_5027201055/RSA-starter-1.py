89493e4ce8c5715a79f8619d5acdb7bc0bebec9a
StandardAE7 <bfm981@gmail.com>
2022-05-18 16:44:02 +0700

    Tugas 8

---
+++ Tugas8_Banabil Fawazaim Muhammad_5027201055/RSA-starter-1.py
@@ -0,0 +1,6 @@
+base=101
+exponent=17
+modulus=22663
+
+k=pow(base,exponent,modulus)
+print(k)
---
+++ Tugas8_Banabil Fawazaim Muhammad_5027201055/RSA-starter-2.py
@@ -0,0 +1,8 @@
+base=12
+exponent=65537
+p=17
+q=23
+modulus=p*q
+
+k=pow(base,exponent,modulus)
+print(k)
